19eaf7f0b03b79d362dc8064fe9b472a52da4c34
PANDILLAPALLI SHIVA SHANKAR <ssp>
2022-07-18 21:03:35 -0300

    tour pass module completed

--- Web Application/src/App.js
+++ Web Application/src/App.js
@@ -6,86 +6,96 @@ import Login from "./components/Login";
 import Signup from "./components/Signup";
 import ForgotPassword from "./components/ForgotPassword";
 import { isLoggedIn } from "./utility/common";
+import Pass from "./Recommendations/Pass";
 
 const PrivateRoute = ({ children }) => {
-  return isLoggedIn() ? children : <Navigate to="/login" replace={true} />;
+    return isLoggedIn() ? children : <Navigate to="/login" replace={true} />;
 };
 
 const PublicRoute = ({ children }) => {
-  return isLoggedIn() ? (
-    <Navigate to="/preferences" replace={true} />
-  ) : (
-    children
-  );
+    return isLoggedIn() ? (
+        <Navigate to="/preferences" replace={true} />
+    ) : (
+        children
+    );
 };
 
 const App = () => {
-  return (
-    <div className="App">
-      <Routes>
-        <Route
-          exact
-          path="/"
-          element={
-            <PublicRoute>
-              <Rooms />
-            </PublicRoute>
-          }
-        />
-        <Route path="/preferences" element={<Preferences />} />
-        <Route
-          exact
-          path="/login"
-          element={
-            <PublicRoute>
-              <Login />
-            </PublicRoute>
-          }
-        />
-        <Route
-          exact
-          path="/signup"
-          element={
-            <PublicRoute>
-              <Signup />
-            </PublicRoute>
-          }
-        />
-        <Route
-          exact
-          path="/forgot-password"
-          element={
-            <PublicRoute>
-              <ForgotPassword />
-            </PublicRoute>
-          }
-        />
+    return (
+        <div className="App">
+            <Routes>
+                <Route
+                    exact
+                    path="/"
+                    element={
+                        <PublicRoute>
+                            <Rooms />
+                        </PublicRoute>
+                    }
+                />
+                <Route path="/preferences" element={<Preferences />} />
+                <Route path="/pass" element={<Pass />} />
+                <Route
+                    exact
+                    path="/login"
+                    element={
+                        <PublicRoute>
+                            <Login />
+                        </PublicRoute>
+                    }
+                />
+                <Route
+                    exact
+                    path="/signup"
+                    element={
+                        <PublicRoute>
+                            <Signup />
+                        </PublicRoute>
+                    }
+                />
+                <Route
+                    exact
+                    path="/forgot-password"
+                    element={
+                        <PublicRoute>
+                            <ForgotPassword />
+                        </PublicRoute>
+                    }
+                />
 
-        <Route
-          exact
-          path="/rooms"
-          element={
-            <PublicRoute>
-              <Rooms />
-            </PublicRoute>
-          }
-        />
+                <Route
+                    exact
+                    path="/rooms"
+                    element={
+                        <PublicRoute>
+                            <Rooms />
+                        </PublicRoute>
+                    }
+                />
 
-        <Route
-          exact
-          path="/user/bookings"
-          element={
-            <PublicRoute>
-              <UserRoomBookings />
-            </PublicRoute>
-          }
-        />
+                <Route
+                    exact
+                    path="/user/bookings"
+                    element={
+                        <PublicRoute>
+                            <UserRoomBookings />
+                        </PublicRoute>
+                    }
+                />
 
-        <Route exact path="/user/bookings" element={<UserRoomBookings />} />
-        <Route exact path="*" element={<div>404, Page Not Found!</div>} />
-      </Routes>
-    </div>
-  );
+                <Route
+                    exact
+                    path="/user/bookings"
+                    element={<UserRoomBookings />}
+                />
+                <Route
+                    exact
+                    path="*"
+                    element={<div>404, Page Not Found!</div>}
+                />
+            </Routes>
+        </div>
+    );
 };
 
 export default App;
--- Web Application/src/Recommendations/Preferences.jsx
+++ Web Application/src/Recommendations/Preferences.jsx
@@ -1,82 +1,97 @@
 import "bootstrap/dist/css/bootstrap.css";
 import { useState } from "react";
+import { Navigate } from "react-router-dom";
+import { isLoggedIn } from "./../utility/common";
+
 const Preferences = () => {
-  const [preferences, setPreferences] = useState({
-    stay: 0,
-    activity: { outdoor: false, indore: false, aviation: false, water: false },
-    interest: { natural: false, historical: false, entertainment: false },
-    budget: 0,
-    distance: 0,
-  });
+    const [preferences, setPreferences] = useState({
+        stay: 0,
+        activity: {
+            outdoor: false,
+            indore: false,
+            aviation: false,
+            water: false,
+        },
+        interest: { natural: false, historical: false, entertainment: false },
+        budget: 0,
+        distance: 0,
+    });
 
-  const handleChange = ({ currentTarget: input }) => {
-    let pref = { ...preferences };
-    if (input.tagName === "SELECT") {
-      // console.log(input.name);
-      pref[input.name] = parseInt(input.value);
-    } else {
-      let names = input.name.split(".");
-      pref[names[0]][names[1]] = !pref[names[0]][names[1]];
-    }
-    setPreferences(pref);
-  };
+    const handleChange = ({ currentTarget: input }) => {
+        let pref = { ...preferences };
+        if (input.tagName === "SELECT") {
+            // console.log(input.name);
+            pref[input.name] = parseInt(input.value);
+        } else {
+            let names = input.name.split(".");
+            pref[names[0]][names[1]] = !pref[names[0]][names[1]];
+        }
+        setPreferences(pref);
+    };
 
-  return (
-    <div className="container">
-      <div className="row">
-        <div className="col-3 p-3 text-end">
-          <label htmlFor="stay" className="form-label p-3">
-            Duration of Stay :
-          </label>
-        </div>
-        <div className="col-5 p-3">
-          <div className="p-3 px-2">
-            <select
-              name="stay"
-              className="form-select"
-              onChange={handleChange}
-              value={preferences.stay}
-            >
-              <option value="1">1 Day</option>
-              <option value="2">2 Days</option>
-              <option value="3">3 or more</option>
-            </select>
-          </div>
-        </div>
-      </div>
-      <div className="row">
-        <div className="col-3 p-3 text-end">
-          <label htmlFor="activityType" className="form-label p-3">
-            Activity Type :
-          </label>
-        </div>
-        <div className="col-5 p-3">
-          <div id="activityType" className="p-3">
-            <div className="form-check p-3 pt-1">
-              <input
-                className="form-check-input"
-                type="checkbox"
-                name="activity.outdoor"
-                onChange={handleChange}
-                checked={preferences.activity.outdoor}
-              />
-              <label className="form-check-label" htmlFor="flexCheckDefault">
-                Outdoor
-              </label>
+    return (
+        // isLoggedIn() && (
+        <div className="container">
+            <div className="row">
+                <div className="col-3 p-3 text-end">
+                    <label htmlFor="stay" className="form-label p-3">
+                        Duration of Stay :
+                    </label>
+                </div>
+                <div className="col-5 p-3">
+                    <div className="p-3 px-2">
+                        <select
+                            name="stay"
+                            className="form-select"
+                            onChange={handleChange}
+                            value={preferences.stay}
+                        >
+                            <option value="1">1 Day</option>
+                            <option value="2">2 Days</option>
+                            <option value="3">3 or more</option>
+                        </select>
+                    </div>
+                </div>
             </div>
-            <div className="form-check p-3">
-              <input
-                className="form-check-input"
-                type="checkbox"
-                name="activity.indore"
-                onChange={handleChange}
-                checked={preferences.activity.indore}
-              />
-              <label className="form-check-label" htmlFor="flexCheckChecked">
-                Indore
-              </label>
-            </div>
-            {/* <div className="form-check p-3">
+            <div className="row">
+                <div className="col-3 p-3 text-end">
+                    <label htmlFor="activityType" className="form-label p-3">
+                        Activity Type :
+                    </label>
+                </div>
+                <div className="col-5 p-3">
+                    <div id="activityType" className="p-3">
+                        <div className="form-check p-3 pt-1">
+                            <input
+                                className="form-check-input"
+                                type="checkbox"
+                                name="activity.outdoor"
+                                onChange={handleChange}
+                                checked={preferences.activity.outdoor}
+                            />
+                            <label
+                                className="form-check-label"
+                                htmlFor="flexCheckDefault"
+                            >
+                                Outdoor
+                            </label>
+                        </div>
+                        <div className="form-check p-3">
+                            <input
+                                className="form-check-input"
+                                type="checkbox"
+                                name="activity.indore"
+                                onChange={handleChange}
+                                checked={preferences.activity.indore}
+                            />
+                            <label
+                                className="form-check-label"
+                                htmlFor="flexCheckChecked"
+                            >
+                                Indore
+                            </label>
+                        </div>
+                        {/* <div className="form-check p-3">
               <input
                 className="form-check-input"
                 type="checkbox"
@@ -100,97 +115,107 @@ const Preferences = () => {
                 Water
               </label>
             </div> */}
-          </div>
-        </div>
-      </div>
+                    </div>
+                </div>
+            </div>
 
-      <div className="row">
-        <div className="col-3 p-3 text-end">
-          <label htmlFor="InterestType" className="form-label p-3">
-            Type of Interests :
-          </label>
-        </div>
-        <div className="col-5 p-3">
-          <div id="InterestType" className="p-3">
-            <div className="form-check p-3 pt-1">
-              <input
-                className="form-check-input"
-                type="checkbox"
-                name="interest.natural"
-                onChange={handleChange}
-              />
-              <label className="form-check-label" htmlFor="flexCheckDefault">
-                Natural Places
-              </label>
+            <div className="row">
+                <div className="col-3 p-3 text-end">
+                    <label htmlFor="InterestType" className="form-label p-3">
+                        Type of Interests :
+                    </label>
+                </div>
+                <div className="col-5 p-3">
+                    <div id="InterestType" className="p-3">
+                        <div className="form-check p-3 pt-1">
+                            <input
+                                className="form-check-input"
+                                type="checkbox"
+                                name="interest.natural"
+                                onChange={handleChange}
+                            />
+                            <label
+                                className="form-check-label"
+                                htmlFor="flexCheckDefault"
+                            >
+                                Natural Places
+                            </label>
+                        </div>
+                        <div className="form-check p-3">
+                            <input
+                                className="form-check-input"
+                                type="checkbox"
+                                name="interest.historical"
+                                onChange={handleChange}
+                            />
+                            <label
+                                className="form-check-label"
+                                htmlFor="flexCheckChecked"
+                            >
+                                Historical Places
+                            </label>
+                        </div>
+                        <div className="form-check p-3">
+                            <input
+                                className="form-check-input"
+                                type="checkbox"
+                                name="interest.entertainment"
+                                onChange={handleChange}
+                            />
+                            <label
+                                className="form-check-label"
+                                htmlFor="flexCheckDefault"
+                            >
+                                Entertainment
+                            </label>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className="form-check p-3">
-              <input
-                className="form-check-input"
-                type="checkbox"
-                name="interest.historical"
-                onChange={handleChange}
-              />
-              <label className="form-check-label" htmlFor="flexCheckChecked">
-                Historical Places
-              </label>
-            </div>
-            <div className="form-check p-3">
-              <input
-                className="form-check-input"
-                type="checkbox"
-                name="interest.entertainment"
-                onChange={handleChange}
-              />
-              <label className="form-check-label" htmlFor="flexCheckDefault">
-                Entertainment
-              </label>
-            </div>
-          </div>
-        </div>
-      </div>
 
-      <div className="row">
-        <div className="col-3 p-3 text-end">
-          <label htmlFor="budget" className="form-label p-3">
-            Budget Range :
-          </label>
+            <div className="row">
+                <div className="col-3 p-3 text-end">
+                    <label htmlFor="budget" className="form-label p-3">
+                        Budget Range :
+                    </label>
+                </div>
+                <div className="col-5 p-3">
+                    <div className="p-3 px-2">
+                        <select
+                            className="form-select"
+                            name="budget"
+                            onChange={handleChange}
+                        >
+                            <option value="1">100$-250$</option>
+                            <option value="2">250$-500$</option>
+                            <option value="3">500$ or more</option>
+                        </select>
+                    </div>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-3 p-3 text-end">
+                    <label htmlFor="distance" className="form-label p-3">
+                        Distance Range :
+                    </label>
+                </div>
+                <div className="col-5 p-3">
+                    <div className="p-3 px-2">
+                        <select
+                            className="form-select"
+                            name="distance"
+                            onChange={handleChange}
+                        >
+                            <option value="1">0-25 Miles</option>
+                            <option value="2">25-50 Miles</option>
+                            <option value="3">50-100 Miles</option>
+                        </select>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div className="col-5 p-3">
-          <div className="p-3 px-2">
-            <select
-              className="form-select"
-              name="budget"
-              onChange={handleChange}
-            >
-              <option value="1">100$-250$</option>
-              <option value="2">250$-500$</option>
-              <option value="3">500$ or more</option>
-            </select>
-          </div>
-        </div>
-      </div>
-      <div className="row">
-        <div className="col-3 p-3 text-end">
-          <label htmlFor="distance" className="form-label p-3">
-            Distance Range :
-          </label>
-        </div>
-        <div className="col-5 p-3">
-          <div className="p-3 px-2">
-            <select
-              className="form-select"
-              name="distance"
-              onChange={handleChange}
-            >
-              <option value="1">0-25 Miles</option>
-              <option value="2">25-50 Miles</option>
-              <option value="3">50-100 Miles</option>
-            </select>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+        // ) || (!isLoggedIn() && <Navigate to="/" replace />)
+    );
 };
 
 export default Preferences;

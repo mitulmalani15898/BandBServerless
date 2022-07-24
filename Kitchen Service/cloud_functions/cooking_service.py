ee8bbc6e8bf21c10ba6da82e3baa9c805d5dcaad
Qurram Zaheer <qurram.zaheer@gmail.com>
2022-07-24 20:38:15 -0300

    lambda updates

--- Kitchen Service/cloud_functions/cooking_service.py
+++ Kitchen Service/cloud_functions/cooking_service.py
@@ -1,11 +1,8 @@
 import base64
 import json
-import uuid
 from datetime import datetime
 import os
 
-from google.cloud import pubsub_v1
-from google.oauth2 import service_account
 import boto3
 from dotenv import load_dotenv
 
@@ -15,7 +12,6 @@ def hello_pubsub(event, context):
     Args:
          event (dict): Event payload.
          context (google.cloud.functions.Context): Metadata for the event.
-
     """
     load_dotenv()
     session = boto3.Session(
@@ -25,45 +21,40 @@ def hello_pubsub(event, context):
     )
     dynamodb = session.resource('dynamodb', region_name='us-east-1')
     orders_table = dynamodb.Table('kitchen_orders')
-
-    PROJECT_ID = 'csci-5410-s22-353123'
-    TOPIC_ID = 'cooking_service'
+    inventory_table = dynamodb.Table('kitchen_inventory')
     pubsub_message = base64.b64decode(event['data']).decode('utf-8')
     msg_json = json.loads(pubsub_message)
     print(msg_json)
 
-    order_id = str(uuid.uuid4())
     try:
-        credentials = service_account.Credentials.from_service_account_file('key.json')
-        client = pubsub_v1.PublisherClient(credentials=credentials)
-
-        topic_path = client.topic_path(PROJECT_ID, TOPIC_ID)
-
+        items = msg_json['items']
         timestamps = msg_json['timestamps']
-        timestamps['confirmed'] = datetime.now().isoformat()
+        user_id = msg_json['user_id']
+        order_id = msg_json['order_id']
 
-        data_json = {
-            'items': msg_json['items'],
-            'timestamps': timestamps,
-            'user_id': msg_json['user_id'],
-            'order_id': order_id
-        }
+        for item in items:
+            print(f'ITEM: {item}')
+            item_inventory = inventory_table.get_item(
+                Key={'_id': item['_id']})['Item']
+            delta = item_inventory['inventory_value'] - item['quantity']
 
-        bytes_data = str(json.dumps(data_json)).encode('utf-8')
-        api_future = client.publish(topic_path, bytes_data)
-        message_id = api_future.result()
+            inventory_table.update_item(
+                Key={'_id': item['_id']}, UpdateExpression='SET inventory_value = :val1',
+                ExpressionAttributeValues={
+                    ':val1': delta
+                }
+            )
 
-        print(f"Published {data_json} to {topic_path}: {message_id}")
+        timestamps['delivered'] = datetime.now().isoformat()
         orders_table.put_item(
             Item={"_id": order_id, "items": msg_json['items'], "user_id": msg_json['user_id'], "timestamps": timestamps,
-                  "status": "confirmed", "detail": "Success"})
+                  "status": "delivered", "detail": "Success"})
 
     except Exception as e:
         print(f"[error]: {e}")
+        order_id = msg_json['order_id']
         timestamps = msg_json['timestamps']
         timestamps['failed'] = datetime.now().isoformat()
         orders_table.put_item(
             Item={"_id": order_id, "items": msg_json['items'], "user_id": msg_json['user_id'], "timestamps": timestamps,
                   "status": "failed", "detail": "Something bad has happened"})
-
-
--- Kitchen Service/lambda/publish_kitchen_order.py
+++ Kitchen Service/lambda/publish_kitchen_order.py
@@ -1,23 +1,27 @@
 import json
+from datetime import datetime
 
 from google.cloud import pubsub_v1
 from google.oauth2 import service_account
 import boto3
 
+
 dynamodb = boto3.resource('dynamodb')
-inventory_table = dynamodb.Table('kitchen_inventory')
 
 
 def lambda_handler(event, context):
     try:
         project_id = event['project_id']
         topic_id = event['topic_id']
-        timestamp = event['timestamp']
         items = event['items']
         user_id = event['user_id']
+        cost = event['cost']
+        BookingNumber = event['BookingNumber']
+        timestamp = datetime.now().isoformat()
 
         """Publishes a message to a Pub/Sub topic."""
-        credentials = service_account.Credentials.from_service_account_file('key.json')
+        credentials = service_account.Credentials.from_service_account_file(
+            'key.json')
         # Initialize a Publisher client.
         client = pubsub_v1.PublisherClient(credentials=credentials)
         # Create a fully qualified identifier of form `projects/{project_id}/topics/{topic_id}`
@@ -25,9 +29,11 @@ def lambda_handler(event, context):
 
         # Data sent to Cloud Pub/Sub must be a bytestring.
         data_json = {
-            'timestamp': timestamp,
+            'timestamps': {'created': timestamp},
             'items': items,
-            'user_id': user_id
+            'user_id': user_id,
+            'cost': cost,
+            'BookingNumber': event['BookingNumber']
         }
 
         bytes_data = str(json.dumps(data_json)).encode('utf-8')
@@ -45,7 +51,11 @@ def lambda_handler(event, context):
     except KeyError:
         return {
             'statusCode': 500,
-            'err': 'Missing parameters, need <project_id>, <topic_id>, <timestamp>, <items>, <user_id>'
+            'err': 'Missing parameters, need <project_id>, <topic_id>, <items>, <user_id> ,<cost>, <BookingNumber>'
         }
 
-
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'err': f'Something went wrong: {e}'
+        }
